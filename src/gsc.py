90531e2389246512e598a54a031d898ea1bb20b8
Stefan Fischerländer <stefan@fischerlaender.de>
2021-11-14 16:27:58 +0100

    gsc.py: 'query', several bugs

--- src/gsc.py
+++ src/gsc.py
@@ -7,6 +7,9 @@ USAGE = """Usage:
         
     ./gsc.py classify [monthly|yearly|all] CLASS_FILE
         CLASS_FILE      a CSV file with two columns: Term,Class, e.g. car,vehicle
+        
+    ./gsc.py query QUERY
+        QUERY           match this query
     """
 
 
@@ -137,40 +140,89 @@ class GSC():
         return term_class_dict
 
 
-    def classify(self, class_file):
+    def classify(self, class_file, granularity):
         """
         Classifies the queries in the database according to the given class file
         """
         terms = self.read_class_file(class_file)
-        data_range = self.get_data_range()
         classes = {}
         db_conn = sqlite3.connect(self.DB_NAME)
         db_cursor = db_conn.cursor()
         i = 0
         for term in terms:
             print(term, terms[term], file=sys.stderr)
-            db_cursor.execute('''
-                SELECT SUBSTR(gsc_date,0,8) AS date_range, SUM(clicks), SUM(impressions)
-                FROM gsc_queries, gsc_results
-                WHERE gsc_queries.id = gsc_results.gsc_query
-                    AND gsc_queries.gsc_query LIKE ?
-                GROUP BY date_range ORDER BY date_range ASC
-            ''', ('%' + term + '%', ) )
+            if granularity == 'monthly':
+                sql = '''
+                    SELECT SUBSTR(gsc_date,0,7) AS date_range, SUM(clicks), SUM(impressions)
+                    FROM gsc_queries, gsc_results
+                    WHERE gsc_queries.id = gsc_results.gsc_query
+                        AND gsc_queries.gsc_query LIKE ?
+                    GROUP BY date_range ORDER BY date_range ASC
+                '''
+            elif granularity == 'yearly':
+                sql = '''
+                    SELECT SUBSTR(gsc_date,0,4) AS date_range, SUM(clicks), SUM(impressions)
+                    FROM gsc_queries, gsc_results
+                    WHERE gsc_queries.id = gsc_results.gsc_query
+                        AND gsc_queries.gsc_query LIKE ?
+                    GROUP BY date_range ORDER BY date_range ASC
+                '''
+            else:
+                sql = '''
+                    SELECT SUM(clicks), SUM(impressions)
+                    FROM gsc_queries, gsc_results
+                    WHERE gsc_queries.id = gsc_results.gsc_query
+                        AND gsc_queries.gsc_query LIKE ?
+                '''
+            db_cursor.execute(sql, ('%' + term + '%', ) )
             db_res = db_cursor.fetchall()
             for res in db_res:
                 for my_class in terms[term]:
-                    my_range = res[0]
-                    if my_class not in classes:
-                        classes[my_class] = {}
-                    if my_range not in classes[my_class]:
-                        classes[my_class][my_range] = { 'clicks': 0, 'impressions': 0}
-                    classes[my_class][my_range]['clicks'] += res[1]
-                    classes[my_class][my_range]['impressions'] += res[2]
+                    if len(res) >= 3:
+                        if my_class not in classes:
+                            classes[my_class] = {}
+                        my_range = res[0]
+                        if my_range not in classes[my_class]:
+                            classes[my_class][my_range] = { 'clicks': 0, 'impressions': 0}
+                        classes[my_class][my_range]['clicks'] += res[1]
+                        classes[my_class][my_range]['impressions'] += res[2]
+                    else:
+                        if my_class not in classes:
+                            classes[my_class] = { 'clicks': 0, 'impressions': 0}
+                        classes[my_class]['clicks'] += res[0]
+                        classes[my_class]['impressions'] += res[1]
             i += 1
+            if i >= 3:
+                break
         db_conn.close()
         print(json.dumps(classes, sort_keys=True, indent=4))
 
-        
+
+    def query(self, my_query):
+        """
+        """
+        db_conn = sqlite3.connect(self.DB_NAME)
+        db_cursor = db_conn.cursor()
+        sql = '''
+            SELECT
+                gsc_queries.gsc_query,
+                SUM(clicks),
+                SUM(impressions) AS sum_impressions,
+                AVG(position)
+            FROM gsc_queries, gsc_results
+            WHERE
+                gsc_queries.gsc_query LIKE ? AND
+                gsc_queries.id = gsc_results.gsc_query
+            GROUP BY gsc_results.gsc_query
+            ORDER BY sum_impressions DESC
+            LIMIT 50
+        '''
+        db_cursor.execute(sql, (my_query,) )
+        db_res = db_cursor.fetchall()
+        for res in db_res:
+            print(res)
+        db_conn.close()
+    
     
     
     def get_data_range(self):
@@ -183,6 +235,50 @@ class GSC():
         date_range = db_cursor.fetchone()
         db_conn.close()
         return date_range
+
+
+
+
+def soundex(s):
+    """
+    https://stackoverflow.com/a/67197882/2771733
+    """
+    if not s:
+        return ""
+    s = unicodedata.normalize("NFKD", s)
+    s = s.upper()
+    replacements = (
+        ("BFPV", "1"),
+        ("CGJKQSXZ", "2"),
+        ("DT", "3"),
+        ("L", "4"),
+        ("MN", "5"),
+        ("R", "6"),
+    )
+    result = [s[0]]
+    count = 1
+    # find would-be replacment for first character
+    for lset, sub in replacements:
+        if s[0] in lset:
+            last = sub
+            break
+    else:
+        last = None
+    for letter in s[1:]:
+        for lset, sub in replacements:
+            if letter in lset:
+                if sub != last:
+                    result.append(sub)
+                    count += 1
+                last = sub
+                break
+        else:
+            if letter != "H" and letter != "W":
+                last = None
+        if count == 4:
+            break
+    result += "0" * (4 - count)
+    return "".join(result)
 
 
 
@@ -200,11 +296,14 @@ if __name__ == '__main__':
             gsc.import_files(file_list)
             
         if my_cmd == 'classify':
-            my_range = sys.argv[2]
+            granularity = sys.argv[2]
             my_class_file = sys.argv[3]
             print("This may take a while, get some fresh coffee ...", file=sys.stderr)
-            gsc.classify(my_class_file)
+            gsc.classify(my_class_file, granularity)
             
+        if my_cmd == 'query':
+            my_query = sys.argv[2]
+            gsc.query(my_query)
         
             
     except ValueError:
